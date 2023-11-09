4c9d710d1a1712819db8b5e58c26c961e72d8e4a
Abdul Aziz <mirzaabdulaziz006@gmail.com>
2023-11-09 12:11:16 -0400

    Initial commit

---
+++ Example_three directories/store_analyze.py
@@ -0,0 +1,19 @@
+import sys,os
+import multiprocessing
+import time
+
+#Creates three directories
+def create_dirs():
+    
+
+def store():
+
+
+    
+def analyze():
+
+
+
+if __name__ == '__main__':
+
+
