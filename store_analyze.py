dc7eee2e5e31e606124ca9794ac0e223347ba1f5
Abdul Aziz <mirzaabdulaziz006@gmail.com>
2023-11-09 12:26:34 -0400

    Reveresed the Insert column as it is being used to give the command to code to insert the data in the storage...

--- store_analyze.py
+++ store_analyze.py
@@ -4,16 +4,21 @@ import time
 
 #Creates three directories
 def create_dirs():
+    if not os.path.exists('./Directories/Fixed'):
+        os.makedirs('./Directories/Fixed')
+    if not os.path.exists('./Directories/Delimited'):
+        os.makedirs('./Directories/Delimited')
+    if not os.path.exists('./Directories/Offset'):
+        os.makedirs('./Directories/Offset')
+
+#Creates three files
+create_dirs()
+# def store():
     
-
-def store():
-
-
-    
-def analyze():
+# def analyze():
 
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
 
