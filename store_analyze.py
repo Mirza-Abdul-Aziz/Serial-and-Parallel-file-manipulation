30002c2d979ee966ec13ad000bf3661e82950582
Abdul Aziz <mirzaabdulaziz006@gmail.com>
2023-11-11 11:08:15 -0400

    Added the functions to write into the pages in a specific format..

--- store_analyze.py
+++ store_analyze.py
@@ -12,27 +12,45 @@ def create_dirs():
         os.makedirs('./Directories/Offset')
 
 def write_to_file(directory, line, page_num):
-    file_path = f'./{directory}/page_{page_num}.txt'
-    with open(file_path, 'a') as f:
+    file_path = f'./Directories/{directory}/page_{page_num}.txt'
+    with open(file_path, 'w') as f:
         f.write(line)
-
+def conversion_for_fixed(values):
+    line = ''
+    for value in values:
+        value = str(value).ljust(20, 'x')
+        line += value
+    return line
+def conversion_for_delimited(values):
+    return '$'.join(map(str, values))
+def conversion_for_offset(values):
+    return ' '.join(map(str, values))
 def store(input_file_path):
     create_dirs()
     page_number = 1
-    counter = 1
+    counter = 0
+    fixed_content = []
+    delimited_content = []
+    offset_content = []
     with open(input_file_path, 'r') as f:
         lines = f.readlines()
         for line in lines:
-            #print(line)
+            line = line.strip().split(',')
             if line[0] == 'INSERT':
-                write_to_file('Fixed', line, page_number)
-                write_to_file('Delimited', line, page_number)
-                write_to_file('Offset', line, page_number)
+                values = list(map(int, line[1:]))
+                fixed_content.append(conversion_for_fixed(values))
+                delimited_content.append('$'.join(map(str, values)))
                 counter += 1 
+                print(counter)
                 if counter == 500:
+                    write_to_file('Fixed', '\n'.join(fixed_content), page_number)
+                    write_to_file('Delimited', '\n'.join(delimited_content), page_number)
                     page_number += 1
-                    counter = 1
-                
+                    counter = 0
+                    fixed_content = []
+                    delimited_content = []
+
+store('./input_data_files/input_1000.txt')                
 
             #     with open('./Directories/Fixed/fixed.txt', 'a') as fixed:
             #         fixed.write(line)
