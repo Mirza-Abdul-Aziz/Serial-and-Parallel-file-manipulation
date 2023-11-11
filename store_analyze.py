57bcb96efd982ea74f8f7921df4510f963c1af34
Abdul Aziz <mirzaabdulaziz006@gmail.com>
2023-11-11 11:22:26 -0400

    Added comments for future references...

--- store_analyze.py
+++ store_analyze.py
@@ -4,6 +4,8 @@ import time
 
 #Creates three directories
 def create_dirs():
+
+    # Check if Directories folder exists
     if not os.path.exists('./Directories/Fixed'):
         os.makedirs('./Directories/Fixed')
     if not os.path.exists('./Directories/Delimited'):
@@ -11,62 +13,85 @@ def create_dirs():
     if not os.path.exists('./Directories/Offset'):
         os.makedirs('./Directories/Offset')
 
+# Writes line to file
 def write_to_file(directory, line, page_num):
+    
+
     file_path = f'./Directories/{directory}/page_{page_num}.txt'
     with open(file_path, 'w') as f:
         f.write(line)
-def conversion_for_fixed(values):
+
+# Convert values to string and left justify them with 'x' to make them 20 characters long
+def conversion_for_fixed(attributes):
+
+    # Initialize empty string
     line = ''
-    for value in values:
-        value = str(value).ljust(20, 'x')
-        line += value
+
+    # Iterate over each attribute
+    for attribute in attributes:
+        # ljust() left justifies the string with 'x' to make it 20 characters long
+        attribute = str(attribute).ljust(20, 'x')
+        
+        # Append attribute to line
+        line += attribute
     return line
-def conversion_for_delimited(values):
-    return '$'.join(map(str, values))
+
 def conversion_for_offset(values):
     return ' '.join(map(str, values))
+
 def store(input_file_path):
+    # Call create_dirs() to create directories
     create_dirs()
-    page_number = 1
-    counter = 0
-    fixed_content = []
-    delimited_content = []
-    offset_content = []
+
+    # Initialize variables
+    page_number = 1 # Page number
+    counter = 0 # Counter to keep track of number of records in a page
+    fixed_content = [] # List to store 500 fixed length records
+    delimited_content = [] # List to store 500 delimited records
+    offset_content = [] # List to store 500 offset records
+    
+    # Read input file
     with open(input_file_path, 'r') as f:
+        # Read lines from input file
         lines = f.readlines()
+
+        # Iterate over lines
         for line in lines:
+
+            # Strip line and split it by comma
             line = line.strip().split(',')
+
+            # Check if operation is INSERT
             if line[0] == 'INSERT':
+
+                # Convert values to int
                 values = list(map(int, line[1:]))
+                
+                # Append values to fixed_content after converting them to specified format
                 fixed_content.append(conversion_for_fixed(values))
+
+                # Append values to delimited_content after converting them to specified format
                 delimited_content.append('$'.join(map(str, values)))
-                counter += 1 
-                print(counter)
+                
+                # Increment counter
+                counter += 1
+
+                # Check if counter is 500 then write fixed_content, delimited_content and offset_content to files and reset counter and lists
                 if counter == 500:
+                    
+                    # Write fixed_content, delimited_content and offset_content to files
                     write_to_file('Fixed', '\n'.join(fixed_content), page_number)
                     write_to_file('Delimited', '\n'.join(delimited_content), page_number)
+                    
+                    # Increment page number to write to next page
                     page_number += 1
+
+                    # Reset counter and lists
                     counter = 0
                     fixed_content = []
                     delimited_content = []
+                    offset_content = []
 
 store('./input_data_files/input_1000.txt')                
 
-            #     with open('./Directories/Fixed/fixed.txt', 'a') as fixed:
-            #         fixed.write(line)
-            # elif line[0] == 'D':
-            #     with open('./Directories/Delimited/delimited.txt', 'a') as delimited:
-            #         delimited.write(line)
-            # elif line[0] == 'O':
-            #     with open('./Directories/Offset/offset.txt', 'a') as offset:
-            #         offset.write(line)
-            # else:
-            #     print("Error: Invalid line in input file.
-    
-# def analyze():
-
-
-
 # if __name__ == '__main__':
-
-
